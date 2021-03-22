a454852d8c0a4ef76d3407df87dfeb8572c8f3d8
anand <rajanand2405@gmail.com>
2021-03-23 00:18:52 +0530

    init commit

---
+++ dist/JAVAproject.bat
@@ -0,0 +1,1 @@
+java -jar "JAVAproject.jar"
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package JAVAproject;
+
+/**
+ *
+ * @author User1
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginForm().setVisible(true);
+            }
+        });
+        // TODO code application logic here
+    }
+
+}
